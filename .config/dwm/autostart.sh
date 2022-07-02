ead5d6f306c0a966e5dd4df826650f88ecd9347b
alexleikin <alex.leeken@gmail.com>
2022-07-01 22:39:59 -0400

    changed autostart

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,7 +1,36 @@
+
+
 pkill picom && picom -f &
+
 urxvtd -q &
-imwheel &
-copyq &
-flameshot &
-unclutter &
-pasystray &
+
+# Terminate already running bar instances
+if pgrep -x flameshot >/dev/null
+   then :
+else
+   flameshot &
+fi
+
+if pgrep -x pasystray >/dev/null
+   then :
+else
+   pasystray &
+fi
+
+if pgrep -x imwheel >/dev/null
+   then :
+else
+   imwheel &
+fi
+
+if pgrep -x unclutter >/dev/null
+   then :
+else
+   unclutter &
+fi
+
+if pgrep -x copyq >/dev/null
+   then :
+else
+   copyq &
+fi
